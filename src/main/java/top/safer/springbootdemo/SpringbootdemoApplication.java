8201eede50a32e750793cd9dbdb990431ac11ee4
SharonLi123 <1627518968@qq.com>
2021-02-03 18:48:53 +0800

    上传文件

--- src/main/java/top/safer/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/top/safer/springbootdemo/SpringbootdemoApplication.java
@@ -1,8 +1,15 @@
 package top.safer.springbootdemo;
 
+import org.apache.coyote.http11.AbstractHttp11Protocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
+/**
+ * @author sakura
+ */
 @SpringBootApplication
 public class SpringbootdemoApplication {
 
@@ -10,4 +17,16 @@ public class SpringbootdemoApplication {
 		SpringApplication.run(SpringbootdemoApplication.class, args);
 	}
 
+
+	@Bean
+	public TomcatServletWebServerFactory tomcatEmbedded() {
+		// 主要为了解决上传文件大于100M出现连接重置的问题
+		TomcatServletWebServerFactory tomcat=new TomcatServletWebServerFactory();
+		tomcat.addConnectorCustomizers(connector->{
+			if((connector.getProtocolHandler() instanceof AbstractHttp11Protocol<?>)){
+				((AbstractHttp11Protocol<?>)connector.getProtocolHandler()).setMaxSwallowSize(-1);
+			}
+		});
+		return tomcat;
+	}
 }
